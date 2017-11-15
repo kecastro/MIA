a2a85be8fd05be428c3be622688379a8abf1720d
Kevin <keacastropo@unal.edu.co>
2017-11-15 09:51:33 -0500

    Edit person

--- mia/static/mia/js/mia.js
+++ mia/static/mia/js/mia.js
@@ -11,3 +11,15 @@ $("#menu-toggle").click(function(e) {
 //Map inputs
 $('#id_patient-map_0').attr('class','geoposition form-control');
 $('#id_patient-map_1').attr('class','geoposition form-control');
+
+//Edit user
+$("#submit-button-edit").click(
+   function() {
+
+      $('#submit-button-edit').hide();
+
+      $('#hidden-button').removeAttr("hidden");
+      $('#form-fieldset').removeAttr("disabled");
+      $('#submit-button-guardar').removeAttr('disabled', 'disabled');
+   }
+);
